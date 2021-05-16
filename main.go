7508c00df107ed4252515f7bb44d272a7a2a2fe3
Sodhan Manandhar <sodhancha@gmail.com>
2021-05-16 13:28:41 +0545

    Added Book Struct which takes in the scanned database row put it in struct type Book and print it

--- main.go
+++ main.go
@@ -21,6 +21,14 @@ import (
 * Render the API first content in the dynamic web pages
  */
 
+type Book struct {
+	Id     int
+	Isbn   string
+	Title  string
+	Author string
+	Price  float64
+}
+
 func DBConnector() {
 
 	db, err := sql.Open("sqlite3", "./DB/foo.db")
@@ -30,13 +38,30 @@ func DBConnector() {
 
 	sql_query := "SELECT * FROM books"
 
-	_, err = db.Query(sql_query)
+	rows, err := db.Query(sql_query)
 
 	if err != nil {
 		fmt.Println("Error in SQL exec: ", err)
 	}
 
 	defer db.Close()
+
+	for rows.Next() {
+
+		book := Book{}
+		err = rows.Scan(&book.Id, &book.Isbn, &book.Title, &book.Author, &book.Price)
+
+		if err != nil {
+			panic(err)
+		}
+		fmt.Print(book.Id)
+		fmt.Print(book.Isbn)
+		fmt.Print(book.Title)
+		fmt.Print(book.Author)
+		fmt.Print(book.Price)
+		fmt.Println("\n----------------------------\n")
+	}
+	err = rows.Err() // get any error encountered ing iteration
 }
 
 func main() {
